8fd876d3af899b15a9ca5df2223fb326f9b4959d
Skyler Ross <orangesnowfox@gmail.com>
2022-04-09 12:04:21 -0700

    chore: update the flake

--- flake.nix
+++ flake.nix
@@ -15,48 +15,31 @@
   };
 
   outputs = { self, nixpkgs, utils, rust-overlay, ... }:
-    let
-      # note: we need to build on stable too, just not sure how to actually *have* 
-      # nightly available for `cargo +nightly fmt`.
-      rustChannel = "nightly";
-      rustVersion = "latest";
-    in
     utils.lib.eachDefaultSystem
       (system:
         let
-          # Imports
+          overlays = [ (import rust-overlay) ];
           pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              rust-overlay.overlay
-              (self: super: {
-                rustc = self.rust-bin.${rustChannel}.${rustVersion}.default;
-                cargo = self.rust-bin.${rustChannel}.${rustVersion}.default;
-                rustfmt = self.rust-bin.${rustChannel}.${rustVersion}.default;
-              })
-            ];
+            inherit system overlays;
           };
-
-          # Configuration for the non-Rust dependencies
-          buildInputs = with pkgs; [ ];
-          nativeBuildInputs = with pkgs; [ rustc cargo pkgconfig ];
         in
         rec {
           # `nix develop`
           devShell = pkgs.mkShell
             {
-              buildInputs = buildInputs ++ (with pkgs;
+              buildInputs = with pkgs;
                 # Tools you need for development go here.
                 [
-                  pkgs.cargo
-                  pkgs.rustc
-                  pkgs.rustfmt
+                  (rust-bin.selectLatestNightlyWith
+                    (toolchain: toolchain.default.override {
+                      extensions = [ "rust-src" ];
+                    })
+                  )
                   nixpkgs-fmt
                   cargo-outdated
                   cargo-edit
                   cargo-llvm-lines
-                ]);
-              RUST_SRC_PATH = "${pkgs.rust-bin.${rustChannel}.${rustVersion}.rust-src}/lib/rustlib/src/rust/library";
+                ];
             };
         }
       );
